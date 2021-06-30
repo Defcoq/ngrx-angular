ba0f1c2217f6a98cca03965cecde3663b72107a7
Jean Pierre DeffoFotso <defcoq73@yahoo.fr>
2021-06-30 13:12:29 +0200

    step 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,31 @@
-import { TestBed, waitForAsync } from '@angular/core/testing'
-import { createComponentMock } from 'angular-unit-test-helper'
-
-import { AppMaterialModule } from './app-material.module'
-import { AppComponent } from './app.component'
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          AppComponent,
-          createComponentMock('CurrentWeatherComponent'),
-          createComponentMock('CitySearchComponent'),
-        ],
-        imports: [AppMaterialModule],
-      }).compileComponents()
-    })
-  )
-  it(
-    'should create the app',
-    waitForAsync(() => {
-      const fixture = TestBed.createComponent(AppComponent)
-      const app = fixture.debugElement.componentInstance
-      expect(app).toBeTruthy()
-    })
-  )
-  it(
-    'should render title in a h1 tag',
-    waitForAsync(() => {
-      const fixture = TestBed.createComponent(AppComponent)
-      fixture.detectChanges()
-      const compiled = fixture.debugElement.nativeElement
-      expect(compiled.querySelector('span').textContent).toContain('LocalCast Weather')
-    })
-  )
-})
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ngrx-local-weather-app-origin'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ngrx-local-weather-app-origin');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-local-weather-app-origin app is running!');
+  });
+});
